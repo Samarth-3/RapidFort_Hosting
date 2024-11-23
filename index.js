985a885f7077bf44c764f2727c67812b0aa464f6
Samarth-3 <spaliwal_be21@thapar.edu>
2024-11-23 16:18:54 +0530

    name changed2

--- index.js
+++ index.js
@@ -83,8 +83,7 @@ app.use(express.static(path.resolve(__dirname, "build")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
-// module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+} );
