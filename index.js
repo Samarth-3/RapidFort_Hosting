985847a93cadcc0dec706acc653a62187a43a601
Samarth-3 <spaliwal_be21@thapar.edu>
2024-11-23 16:28:30 +0530

    vercel3

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.post(
   }
 );
 
-app.use(express.static(path.resolve(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "build", "index.html"));
