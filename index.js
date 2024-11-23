207a10be012ec39b164398938e0aa240f123377a
Samarth-3 <spaliwal_be21@thapar.edu>
2024-11-23 15:59:39 +0530

    changes2

--- index.js
+++ index.js
@@ -78,11 +78,13 @@ app.post(
   }
 );
 
-app.use(express.static(path.resolve(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "build")));
+
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
